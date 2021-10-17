1cf6b01e4a694c41053c24c20e5dfed278d5a82a
Jake <mapattacker@gmail.com>
2021-10-17 14:07:37 +0800

    Add parallel feture extraction

--- urbansounds/preprocess.py
+++ urbansounds/preprocess.py
@@ -2,9 +2,11 @@
 
 import os
 
+import joblib
 import librosa
 import numpy as np
 import pandas as pd
+from joblib import Parallel, delayed
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
@@ -22,7 +24,28 @@ class preprocess:
         return mfccs_scaled_features
 
 
-    def pipeline(self, audio_dataset_path, metadata_file):
+    def f_extractor(self, df, idx):
+        """some processing before feature extraction"""
+        row = df.iloc[idx]
+        file_name = os.path.join(audio_dataset_path, f'fold{row["fold"]}', row["slice_file_name"])
+        final_class_labels = row["class"]
+        data = self.mfcc_extractor(file_name)
+        return [data, final_class_labels]
+
+    def encoder(self, y):
+        """encode string labels to one-hot & save encode mapping"""
+
+        # encode label string to int
+        labelencoder = LabelEncoder()
+        y = labelencoder.fit_transform(y)
+        joblib.dump(labelencoder, "encoder.jb")
+        # one-hot encoding
+        y = to_categorical(y)
+
+        return y
+
+
+    def pipeline(self, audio_dataset_path, metadata_file, n_jobs=4):
         """preprocess pipeline
         urbansounds metadata consists of the following columns
         ["slice_file_name","fsID","start","end","salience","fold","classID","class"]"""
@@ -32,12 +55,9 @@ class preprocess:
         metadata_path = os.path.join(audio_dataset_path, metadata_file)
         metadata = pd.read_csv(metadata_path)
 
-        extracted_features=[]
-        for index_num, row in tqdm(metadata.iterrows()):
-            file_name = os.path.join(audio_dataset_path, f'fold{row["fold"]}', row["slice_file_name"])
-            final_class_labels = row["class"]
-            data = self.mfcc_extractor(file_name)
-            extracted_features.append([data,final_class_labels])
+        # parallel extractor
+        extracted_features = Parallel(n_jobs=n_jobs)(
+            delayed(self.f_extractor)(metadata, idx) for idx in tqdm(range(len(metadata))))
 
         ## Converting extracted_features to pandas dataframe
         extracted_features_df = pd.DataFrame(extracted_features, columns=["feature","class"])
@@ -45,9 +65,7 @@ class preprocess:
         X = np.array(extracted_features_df["feature"].tolist())
         y = np.array(extracted_features_df["class"].tolist())
 
-        # one-hot encoding for labels
-        labelencoder = LabelEncoder()
-        y = to_categorical(labelencoder.fit_transform(y))
+        y = self.encoder(y)
 
         ## Train Test Split
         X_train, X_test, y_train, y_test = \
