e58c6e13aed9a7d85cdb3a035b6586de0ceea9ba
Jake Teo <mapattacker@gmail.com>
2021-10-21 18:59:34 +0800

    Update download.sh

--- urbansounds/download.sh
+++ urbansounds/download.sh
@@ -1,4 +1,4 @@
 mkdir data
 cd data
-!wget https://zenodo.org/record/1203745/files/UrbanSound8K.tar.gz -O urban8k.tgz
-!tar -xzf urban8k.tgz
+wget https://zenodo.org/record/1203745/files/UrbanSound8K.tar.gz -O urban8k.tgz
+tar -xzf urban8k.tgz
