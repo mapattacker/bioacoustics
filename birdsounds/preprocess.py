942934861ae4c37b9e0862c5435d48aeb1453769
Jake Teo <mapattacker@gmail.com>
2022-02-20 21:16:59 +0800

    Update preprocess.py

--- birdsounds/preprocess.py
+++ birdsounds/preprocess.py
@@ -2,6 +2,9 @@
 https://www.kaggle.com/shtrausslearning/keras-bird-spectogram-multiclass-classification/notebook
 """
 
+import os
+
+from PIL import Image
 import pandas as pd
 import librosa
 from tqdm import tqdm, tnrange, tqdm_notebook
@@ -13,6 +16,7 @@ def split_signal(sig, sr, sl):
     
     sr (int): sampling rate
     sl (int) sampling length"""
+    
     step = int(sample_rate * sample_len)
     sig_splits = []
     for i in range(0, len(sig), step):
@@ -22,6 +26,47 @@ def split_signal(sig, sr, sl):
         sig_splits.append(split)
     return sig_splits
 
+
+def get_spectrograms(filepath, primary_label, output_dir):
+    """extracts spectrograms and saves them in a working directory"""
+    
+    # duration is set from global variable
+    sig, rate = librosa.load(filepath, sr=coefs.sr, offset=None, duration=coefs.cutoff)
+    sig_splits = split_signal(sig) # split the signal into parts
+    
+    # Extract mel spectrograms for each audio chunk
+    s_cnt = 0
+    saved_samples = []
+    for chunk in sig_splits:
+        
+        hop_length = int(coefs.sl * coefs.sr / (coefs.sshape[1] - 1))
+        mel_spec = librosa.feature.melspectrogram(y=chunk, 
+                                                  sr=coefs.sr, 
+                                                  n_fft=1024, 
+                                                  hop_length=hop_length, 
+                                                  n_mels=coefs.sshape[0], 
+                                                  fmin=coefs.fmin, 
+                                                  fmax=coefs.fmax)
+    
+        mel_spec = librosa.power_to_db(mel_spec**2, ref=np.max) 
+        
+        # Normalize
+        mel_spec -= mel_spec.min()
+        mel_spec /= mel_spec.max()
+        
+        # Save as image file
+        save_dir = os.path.join(output_dir, primary_label)
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        save_path = os.path.join(save_dir, filepath.rsplit(os.sep, 1)[-1].rsplit('.', 1)[0] + 
+                                 '_' + str(s_cnt) + '.png')
+        im = Image.fromarray(mel_spec * 255.0).convert("L")
+        im.save(save_path)
+        
+        saved_samples.append(save_path)
+        s_cnt += 1
+        
+    return saved_samples
 
 
 
